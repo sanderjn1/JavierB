29eec0a66d3f8ec4de7409d4e3a0a71d3e3c779c
sanderjn1 <112398478+sanderjn1@users.noreply.github.com>
2023-09-28 13:41:16 +0200

    endring av oppsett og add enkel html kode

--- JavierB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JavierB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
